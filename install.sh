8e5064830a11b2adb626e117f112f0a12a634cff
viggle-by <barlevsj@gmail.com>
2026-01-11 19:45:27 -0500

    ggex

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ timedatectl set-ntp true
 echo "Available disks:"
 lsblk -dpno NAME,SIZE | grep -E "sd|nvme|vd"
 
-read -rp "Enter disk to install to (e.g. /dev/nvme0n1): " DISK
+read -rp "Enter disk to install to (e.g. /dev/sda): " DISK
 [[ ! -b "$DISK" ]] && echo "Invalid disk." && exit 1
 
 echo "WARNING: This will ERASE $DISK"
@@ -31,8 +31,13 @@ sgdisk --zap-all "$DISK"
 sgdisk -n 1:0:+1G -t 1:ef00 "$DISK"
 sgdisk -n 2:0:0   -t 2:8300 "$DISK"
 
-EFI="${DISK}p1"
-ROOT="${DISK}p2"
+if [[ "$DISK" =~ nvme ]]; then
+  EFI="${DISK}p1"
+  ROOT="${DISK}p2"
+else
+  EFI="${DISK}1"
+  ROOT="${DISK}2"
+fi
 
 mkfs.fat -F32 "$EFI"
 mkfs.ext4 "$ROOT"
@@ -41,45 +46,21 @@ mount "$ROOT" /mnt
 mkdir -p /mnt/boot
 mount "$EFI" /mnt/boot
 
-### PACKAGE LISTS ###
-BASE_PKGS="
-base base-devel linux linux-firmware
-networkmanager sudo
-pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-refind efibootmgr
-git nano
-firefox
-dolphin konsole
-ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji
-kotlin
-"
-
-KDE_PKGS="
-plasma-meta
-kde-applications
-xdg-desktop-portal-kde
-"
-
-COSMIC_PKGS="
-cosmic
-cosmic-session
-"
-
-HYPRLAND_PKGS="
-hyprland
-wayland xorg-xwayland
-wl-clipboard grim slurp swaybg mako
-xdg-desktop-portal-hyprland
-xdg-desktop-portal-gtk
-polkit-kde-agent
-"
-
-### INSTALL ###
+### PACKAGES ###
 pacstrap /mnt \
-  $BASE_PKGS \
-  $KDE_PKGS \
-  $COSMIC_PKGS \
-  $HYPRLAND_PKGS
+  base base-devel linux linux-firmware \
+  networkmanager sudo \
+  pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
+  refind efibootmgr \
+  git nano firefox dolphin konsole \
+  ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji \
+  plasma-meta kde-applications xdg-desktop-portal-kde \
+  cosmic cosmic-session \
+  hyprland wayland xorg-xwayland \
+  wl-clipboard grim slurp swaybg mako \
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+  polkit-kde-agent \
+  kotlin
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -90,7 +71,6 @@ set -e
 ### TIME / LOCALE ###
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
-
 sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen
 locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
@@ -98,12 +78,12 @@ echo "LANG=$LOCALE" > /etc/locale.conf
 ### HOST ###
 echo "$HOSTNAME" > /etc/hostname
 cat <<HOSTS > /etc/hosts
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+127.0.0.1 localhost
+::1       localhost
+127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
 HOSTS
 
-### USERS ###
+### PASSWORDS ###
 echo "Set ROOT password:"
 passwd
 
@@ -117,29 +97,27 @@ sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 systemctl enable NetworkManager
 systemctl enable sddm
 
-### rEFInd INSTALL ###
+### rEFInd ###
 refind-install
 sed -i 's/#scan_all_linux_kernels/scan_all_linux_kernels/' /boot/EFI/refind/refind.conf
 
 PARTUUID=\$(blkid -s PARTUUID -o value $ROOT)
 
 cat <<REFIND > /boot/EFI/refind/refind_linux.conf
-"Arch Linux"  "root=PARTUUID=\$PARTUUID rw quiet loglevel=3"
+"Arch Linux" "root=PARTUUID=\$PARTUUID rw quiet loglevel=3"
 REFIND
 
 ### rEFInd Minecraft THEME ###
 cd /root
 git clone https://github.com/Minecraftian14/rEFInd-Minecraft.git
 cd rEFInd-Minecraft
-
 kotlin Build.kt build bakebg bakebg.osicons=2 bakebg.othericons=1
 
 mkdir -p /boot/EFI/refind/themes
 cp -r build/rEFInd-Minecraft /boot/EFI/refind/themes/minecraft
-
 echo "include themes/minecraft/theme.conf" >> /boot/EFI/refind/refind.conf
 
-### HYPRLAND POLKIT ###
+### POLKIT AUTOSTART ###
 mkdir -p /etc/xdg/autostart
 cat <<POLKIT > /etc/xdg/autostart/polkit-kde-agent.desktop
 [Desktop Entry]
